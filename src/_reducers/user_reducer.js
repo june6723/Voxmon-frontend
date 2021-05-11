8eef2abb82aef12ef10da7c3202e11b3df57a2ec
june6723 <june6723@naver.com>
2021-05-11 14:28:27 +0900

    Edit login

--- src/_reducers/user_reducer.js
+++ src/_reducers/user_reducer.js
@@ -1,17 +1,14 @@
 import {AUTH_USER, LOGIN_USER} from '../_actions/types'
 import {REGISTER_USER} from '../_actions/types' 
+
 export default function(state={}, action){
     switch(action.type) {
         case LOGIN_USER:
                 return { ...state, loginSuccess:action.payload}
-            break;
-
         case REGISTER_USER:
                 return { ...state, RegisterSuccess:action.payload}
-            break;
         case AUTH_USER:
                 return {...state, userData:action.payload}
-            break;
         default:
             return state;
             
